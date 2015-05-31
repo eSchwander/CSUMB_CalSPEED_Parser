18a51043621ab544674caa3f4fa2647c1f8f24e3
Evan Schwander <jazzbo.game@gmail.com>
2015-05-31 11:37:19 -0700

    Removed uneeded file

--- UploadData/README.py
+++
@@ -1,8 +0,0 @@
-'''
-
-This is where data to be parsed is uploaded.
-The files to be parsed are then placed in other folders depending on what kind of test they represent.
-The other folders are created if they do not already exist.
- 	
--Evan Schwander	(eschwander@csumb.edu)	
-'''
